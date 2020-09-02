92472028f4f840518d1d0a839c17155aed2f676c
tlasalle5027 <tlasalle5027@gmail.com>
2020-09-02 18:41:48 -0400

    Set up Spinners

--- app/src/main/java/com/cfmsoftware/billsplitter/MainActivity.kt
+++ app/src/main/java/com/cfmsoftware/billsplitter/MainActivity.kt
@@ -2,15 +2,82 @@ package com.cfmsoftware.billsplitter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+
 import com.facebook.ads.AudienceNetworkAds
 
 class MainActivity : AppCompatActivity() {
+
+    var check:Double = 0.0
+    var tip:Double = 0.0
+    var people:Double = 1.0
+    var total:Double = 0.0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         AudienceNetworkAds.initialize(this)
 
-        
+        val tip_pcnt = resources.getStringArray(R.array.Tip_Pct)
+        val num_ppl = resources.getStringArray(R.array.Num_People)
+
+        val tipSpinner = findViewById<Spinner>(R.id.tipSpinner)
+        if(tipSpinner != null) {
+            val tipAdapter =
+                ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, tip_pcnt)
+            tipSpinner.adapter = tipAdapter
+            tipSpinner.setSelection(0)
+
+            tipSpinner.onItemSelectedListener = object :
+                AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>,
+                    view: View, position: Int, id: Long
+                ) {
+                    when (position) {
+                        0 -> tip = .10
+                        1 -> tip = .12
+                        2 -> tip = .15
+                        3 -> tip = .20
+                        4 -> tip = .25
+                        5 -> tip = .30
+                    }
+                }
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    // write code to perform some action
+                }
+            }
+        }
+
+
+        val pplSpinner = findViewById<Spinner>(R.id.peopleSpinner)
+        if (pplSpinner != null){
+            val pplAdapter = ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, num_ppl)
+            pplSpinner.adapter = pplAdapter
+            pplSpinner.setSelection(0)
+
+            pplSpinner.onItemSelectedListener = object :
+                AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>,
+                    view: View, position: Int, id: Long
+                ) {
+                    when (position) {
+                        0 -> people = 2.0
+                        1 -> people = 3.0
+                        2 -> people = 4.0
+                        3 -> people = 5.0
+                        4 -> people = 6.0
+                    }
+                }
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    // write code to perform some action
+                }
+            }
+        }
     }
 }
